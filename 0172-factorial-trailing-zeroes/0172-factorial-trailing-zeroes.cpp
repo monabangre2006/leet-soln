bce997602ab9bb22e82c6204a530000580ef25fc
monabangre2006 <monabangre2006@gmail.com>
2026-07-24 16:48:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.61%) - LeetHub-Neo

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  
+    int trailingZeroes(int n) {
+        
+        
+        int cnt=0;
+        
+       while(n>0)
+       {
+         n=n/5;
+         cnt+=n;
+       }
+       return cnt;
+       
+    }
+};
