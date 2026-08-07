7199c2f00038f786ad3e00e17d6180faddfcee30
monabangre2006 <monabangre2006@gmail.com>
2026-08-07 10:13:27 +0530

    Time: 0 ms (100%), Space: 28.7 MB (49.62%) - LeetHub-Neo

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+       map<int,int>mpp;
+       for(int i=0;i<nums.size();i++)
+       {
+          mpp[nums[i]]++;
+       }
+       for(int i=0;i<nums.size();i++)
+       {
+        if(nums[i]%2==0 && mpp[nums[i]]==1)
+        {
+            return nums[i];
+        }
+       }
+       return -1;
+    }
+};
